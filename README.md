36abae1184ed878975497587dd56994ebf782682
lanny lopez <lannylynnlopez89@gmail.com>
2021-11-06 14:50:40 -0600

    forgot to push last night

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # workday-scheduler
+In this project I created a workday scheduler using HTML BOOTSTRAP CSS JAVASCRIPT MOMENT.JS
+
+with this project i displayed talents such as saving something to localStorage with a button as well as changing classes with javascript 
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -5,13 +5,38 @@ $(document).ready(function(){
 
 var currentHour = moment().hour();
 
-$(".daily-task").on("click", "p", function(){
-    var text = $(this)
-    .text()
-    .trim();
-    var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
-})
+var checkTime = function() {
+    $(".time-block").each(function(){
+        var blockTime = parseInt($(this).attr("id").split("-")[1])
+        
+        if (blockTime < currentHour){
+            $(this).addClass("past");
+        } else if (blockTime === currentHour){
+            $(this).removeClass("past")
+            $(this).addClass("present")
+        } else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+    
+    });
+};
+
+checkTime();
+var blockCheck = setInterval(checkTime, 30000);
+
+$(".saveBtn").on("click", function(){
+    var description = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, description)
+});
+$("#nine", ".description").val(localStorage.getItem("nine"))
+$("#ten", ".description").val(localStorage.getItem("ten"))
+$("#eleven", ".description").val(localStorage.getItem("eleven"))
+$("#twelve", ".description").val(localStorage.getItem("twelve"))
+$("#one", ".description").val(localStorage.getItem("one"))
+$("#two", ".description").val(localStorage.getItem("two"))
+$("#three", ".description").val(localStorage.getItem("three"))
+$("#four", ".description").val(localStorage.getItem("four"))
+$("#five", ".description").val(localStorage.getItem("five"))
